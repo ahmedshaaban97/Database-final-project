e72641f6f5451c13a4a9a0c178613d1e9af184af
Ahmed <ahmedshaaban97>
2019-01-27 00:05:17 +0200

    views initial folders are added

--- helpers/iu_donner_helper.js
+++ helpers/iu_donner_helper.js
@@ -9,16 +9,9 @@ let sqlConnection = mysql.createConnection({
 let success = 400;
 let fail = 404;
 module.exports = {
-<<<<<<< HEAD
-    insert: function (req, res, fname, lname, age, weight, ssn) {
-        let status = 404;
-        let sql = `INSERT INTO donor(Fname,Lname,age,weight,ssn)VALUES('${fname}','${lname}','${age}','${weight}','${ssn}')`
-        sqlConnection.query(sql, (err, rows, fields) => {
-=======
-    insert: function (req, res, fname, lname, age, condition, ssn, bloodType,weight) {
+    insert: function (req, res, fname, lname, age, condition, ssn, bloodType, weight) {
         let status = 404;
         sqlConnection.query(`INSERT INTO patient(Fname,Lname,age,pcondition,ssn,bloodType,weight)VALUES('${fname}','${lname}','${age}','${condition}','${ssn}','${bloodType}','${weight}')`, (err, rows, fields) => {
->>>>>>> 5b21a917e5eb3c6ea71f6ca5c6a465f3b115cf19
             if (!err) {
                 console.log('patient inserted');
                 status = 400;
@@ -35,12 +28,9 @@ module.exports = {
     },
     update: function (req, res, fname, lname, age, condition, ssn, adress) {
         let status = 404;
-<<<<<<< HEAD
         let sql = `UPDATE donor SET Fname = '${fname}' , Lname = '${lname}' , age = '${age}', pcondition = '${condition}', adress = '${adress}' WHERE ssn = '${ssn}'`
         sqlConnection.query(sql, (err, rows, fileds) => {
-=======
-        sqlConnection.query(`UPDATE donner SET Fname = '${fname}' , Lname = '${lname}' , age = '${age}', pcondition = '${condition}', adress = '${adress}' WHERE ssn = '${ssn}'`, (err, rows, fileds) => {
->>>>>>> 5b21a917e5eb3c6ea71f6ca5c6a465f3b115cf19
+
             if (!err) {
                 console.log('patient updated');
                 status = 400;
@@ -54,8 +44,6 @@ module.exports = {
         });
     },
 
-
-<<<<<<< HEAD
     select: function (req, rse) {
         let status = 404;
         let sql = 'SELECT * FROM donor'
@@ -64,13 +52,6 @@ module.exports = {
                 console.log('table selected');
                 status = 400;
                 console.log(status);
-=======
-    select: function (req, res) {
-        sqlConnection.query('SELECT * FROM donner', (err, rows, fields) => {
-            if (!err)
->>>>>>> 5b21a917e5eb3c6ea71f6ca5c6a465f3b115cf19
-                res.send(rows);
-                return status;
             } else {
                 console.log(err);
                 return fail;
@@ -79,44 +60,39 @@ module.exports = {
     },
 
     select_ssn: function (req, res, ssn) {
-<<<<<<< HEAD
         let sql = 'SELECT * FROM donor where ssn = ?'
         sqlConnection.query(sql, [ssn], (err, rows, fields) => {
-=======
-        sqlConnection.query('SELECT * FROM donner where ssn = ?', [ssn], (err, rows, fields) => {
->>>>>>> 5b21a917e5eb3c6ea71f6ca5c6a465f3b115cf19
+
             if (!err)
                 res.send(rows);
             console.log(err)
         })
     },
 
-    delete_ssn: function (req, res, ssn) {
-<<<<<<< HEAD
+    delete_ssn: function () {
         let sql = 'DELETE FROM donor where ssn = ?'
         sqlConnection.query(sql, [ssn], (err, rows, fields) => {
-=======
-
-        sqlConnection.query('DELETE FROM donner where ssn = ?', [ssn], (err, rows, fields) => {
->>>>>>> 5b21a917e5eb3c6ea71f6ca5c6a465f3b115cf19
             if (!err)
                 res.send('Patient deleted');
             console.log(err)
         })
     },
 
-    testDonner: function () {
-        let sql = 'SELECT * FROM donor'
-        sqlConnection.query(sql, (err, rows, fields) => {
-            if (err) {
-                createDonorsTable();
-                createEmployeeTable();
-                createRecipientTable();
-            }
-            if (!err) {
-                console.log('Database already set and ready for use')
-            }
 
-        });
-    }
+    testDonner:
+
+        function () {
+            let sql = 'SELECT * FROM donor'
+            sqlConnection.query(sql, (err, rows, fields) => {
+                if (err) {
+                    createDonorsTable();
+                    createEmployeeTable();
+                    createRecipientTable();
+                }
+                if (!err) {
+                    console.log('Database already set and ready for use')
+                }
+
+            });
+        }
 };
